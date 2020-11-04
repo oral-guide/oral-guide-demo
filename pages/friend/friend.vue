87335af027fc3eb3b920d5a9eb741ae08fdc4639
HumanSean <847863297@qq.com>
2020-11-04 09:27:39 +0800

    initialized

---
+++ pages/friend/friend.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		This is a friend page.
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ pages/user/user.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		This is a user page.
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
