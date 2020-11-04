0247030eb7779e269a4bf5f741fe0782fb1aacd8
HumanSean <847863297@qq.com>
2020-11-04 13:10:21 +0800

    新增大厅&房间页面，修改主题色等

---
+++ pages/hall/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="hall">
+    这里是大厅，应该有很多房间
+    <van-button class="btn" type="primary" block color="#ff6600">创建房间</van-button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      
+    };
+  },
+};
+</script>
+
+<style scoped>
+.hall {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    box-sizing: border-box;
+    padding: 20px;
+}
+.btn {
+    position: absolute;
+    width: calc(100% - 40px);
+    left: 20px;
+    bottom: 20px;
+}
+</style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <!-- 在这里引入组件 -->
+    <div class="game" v-for="game in games" :key="game.name" @click="navigate">
+      {{ game.name }}
+    </div>
   </div>
 </template>
 
@@ -8,13 +11,36 @@
 export default {
   data() {
     return {
+      games: [
+        { name: "谁是卧底" },
+        { name: "XXX" },
+        { name: "好玩的游戏" },
+        { name: "PK学习" }
+      ]
     };
   },
   onLoad() {},
   methods: {
+    navigate() {
+      console.log(111);
+      uni.navigateTo({ url: "../hall/index",
+        success: (res) => console.log(res),
+        fail: (res) => console.log(res),
+      });
+    }
   },
 };
 </script>
 
 <style>
+.game {
+  position: relative;
+  width: 80%;
+  left: 10%;
+  height: 100px;
+  margin: 20px 0;
+  border: 1px solid #333;
+  border-radius: 3px;
+  text-align: center;
+}
 </style>
