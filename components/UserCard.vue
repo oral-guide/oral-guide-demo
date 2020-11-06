db8b1f39231ea7b507a1f02ebb96b4869d13a04e
SylviaChen23 <1391847132@qq.com>
2020-11-06 11:37:58 +0800

    用户卡的一些添加

---
+++ components/UserCard.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="usercard">
+        <van-row type="flex" justify="space-around">
+            <van-image round width="4rem" height="4rem" src="/static/userimg.jpg"/>
+        </van-row>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return{
+                users:[
+                    {id:01,
+                    name:"A",
+                    isSpy:false,
+                    isAlive:true,
+                    isSpeaking:false,
+                    isOwner:true,
+                    
+                    },
+                    {id:02,
+                    name:"B",
+                    isSpy:true,
+                    isAlive:true,
+                    isSpeaking:false,
+                    isOwner:false
+                    },
+                    {id:03,
+                    name:"C",
+                    isSpy:false,
+                    isAlive:true,
+                    isSpeaking:false,
+                    isOwner:false
+                    },
+                    {id:04,
+                    name:"D",
+                    isSpy:false,
+                    isAlive:true,
+                    isSpeaking:false,
+                    isOwner:false
+                    }
+                ]
+                   
+                
+            };
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- pages/room/index.vue
+++ pages/room/index.vue
@@ -1,12 +1,15 @@
 <template>
-    <div>
-        这里放入各种组件
-    </div>
+  <div>
+    <!-- <user-card></user-card> -->
+  </div>
 </template>
 
 <script>
+    // import userCard from "../../components/UserCard"
     export default {
-        
+        // components:{
+        //     userCard
+        // }
     }
 </script>
 
