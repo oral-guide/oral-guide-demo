3fb3d9ed1799f570cc49a5a7b11a51585e5e5173
lorienn <bonitaxxy@163.com>
2020-11-07 12:13:50 +0800

    新增游戏组件

--- pages/game/index.vue
+++ pages/game/index.vue
@@ -1,15 +1,60 @@
 <template>
     <div>
-        <!-- 这里是游戏界面，在这里放组件 -->
+        <van-notice-bar
+            left-icon="volume-o"
+            text="当前发言的是：玩家1"
+        />
+        <div class="desc">
+            <div class="img">
+                <img src="../../static/library.png" alt="">
+            </div>
+            <div class="word">library</div>
+            <div class="def">A room or set of rooms where books and other literary materials are kept</div>
+        </div>
+        <div class="vote" v-show="isVote">123546</div>
     </div>
 </template>
 
 <script>
     export default {
-        
+        data () {
+            return {
+                isVote: true
+            }
+        }
     }
 </script>
 
 <style lang="scss" scoped>
+.desc {
+    padding-top: 15px;
+    height: 240px;
+    text-align: center;
 
+    .img {
+        height: 150px;
+        margin-bottom: 10px;
+
+        img {
+            display: block;
+            margin: 0 auto;
+            width: 150px;
+            height: 150px;
+        }
+    }
+
+    .word {
+        margin-bottom: 5px;
+        font-size: 20px;
+        font-weight: 700;
+    }
+
+    .def {
+        padding: 0 20px;
+        font-size: 12px;
+    }
+}
+.vote {
+    
+}
 </style>
