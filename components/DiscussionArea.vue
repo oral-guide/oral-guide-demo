11be60174fd663fe27c968cfb802bdf3d3207e9f
SylviaChen23 <1391847132@qq.com>
2020-11-08 21:16:14 +0800

    Discussion

---
+++ components/DiscussionArea.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <van-popup
+      show="{{ show }}"
+      round
+      position="bottom"
+      custom-style="height: 20%"
+      bind:close="onClose"
+    />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style  lang="scss" scoped>
+</style>
